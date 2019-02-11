90e8bd524c252a90a6eee1cce5747b6faaa3281b
reneab <rene.abd@gmail.com>
2019-02-11 23:21:00 +0800

    added event listeners on logging

--- logger.js
+++ logger.js
@@ -2,24 +2,38 @@
 
 const http = require('http');
 const fs = require('fs');
+const events = require('events');
+
+const eventEmitter = new events.EventEmitter();
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const logFile = 'log.txt';
 
+// event listener on opening the log file
+var rs = fs.createReadStream(logFile);
+rs.on('open', function () {
+  console.log('Oh! Someone has opened the log file!');
+});
+
+// simple event listener that performs a log
+eventEmitter.on('request', function() {
+    console.log('Request saved successfully to ' + logFile);
+});
+
 // server that logs the requests in to a logFile
 const server = http.createServer(function(req, res) {
     const toWrite = 'Received request: ' + req.url;
     console.log(toWrite);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-
+    
     fs.appendFile(logFile, toWrite  + '\n', function(err) {
-        if (err) throw err;
-        console.log('Request saved successfully to ' + logFile);
+        if (err) console.error('Could not write to log file');
+        eventEmitter.emit('request');
     });
 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
     res.write('<h1>Thank you from NodeJS!</h1>');
     res.end();
 });
