734c231cf69a889566630f87b1d351bdee24944f
reneab <rene.abd@gmail.com>
2019-03-08 17:44:56 +0800

    add fetching data from array

--- server-express.js
+++ server-express.js
@@ -17,6 +17,21 @@ app.get('/hi/:id', (req, res) => {
     res.send('Hello from Express, ' + req.params.id);
 });
 
+const names = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Johanna'} 
+];
+
+app.get('/name/:id', (req, res) => {
+    console.log('Received request: ' + req.url);
+    console.log(req.params);
+    console.log('---------------------------------------------');
+    const entry = names.find(c => c.id == parseInt(req.params.id));
+    // send 404 if not found
+    if (!entry) res.status(404).send('ID not found in database')
+    else res.send(entry.name);
+});
+
 // login route with query parameters
 app.get('/login', (req, res) => {
     console.log('Received request: ' + req.url);
